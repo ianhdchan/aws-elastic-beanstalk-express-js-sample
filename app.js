9b519706ab52ae172ff4d22ef8da37a082a95a00
Ian Chan <ianchan1108@hotmail.com>
2025-10-30 16:56:39 -0700

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => res.send("Lab 7, here we go!"));
+app.get("/", (req, res) => res.send("Lab 7, PART 2 TEST!"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
